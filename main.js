db14fbff9defd4b8f4919bd0d62525acc583a1d1
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2019-12-13 11:51:41 -0600

    Updated File

--- main.js
+++ main.js
@@ -10,38 +10,3 @@ function displayTime () {
     document.getElementById("MyClock").innerText = time;
     document.getElementById("MyClock").textContent = time;
 }
-
-function determineEquality () {
-    let myArray = [];
-    for (i = 0; i < 10000; i++){
-        myArray.push(i);
-    }
-    return myArray;
-}
-
-function myNumbers () {
-    let myArray2 = [];
-    for (i = 0; i < 100; i++){
-        myArray2.push(Range(2,2000));
-    }
-    return myArray2;
-}
-
-
-function myNumbers () {
-    let myArray2 = [];
-    for (i = 0; i < 100; i++){
-        myArray2.push(Range(2,2000));
-    }
-    return myArray2;
-}
-
-
-
-
-console.log(determineEquality())
-console.log(myNumbers())
-setInterval(displayTime,1000);
-console.log("Will you hire me?")
-console.log("I will exceed all expectations as your newest Software Developer")
-console.log("I am writing this here because I am wondering if you will read my code")
