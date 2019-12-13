8b3cb38b70b92b84245afbe2e536342f932619a4
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2019-12-12 22:25:42 -0600

    Updated File

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // Setup
+
+function displayTime () {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    let time = h + ":" + m + ":" + s;
+    document.getElementById("MyClock").innerHTML = time;
+}
+
+displayTime()
