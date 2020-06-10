f85ba0a9ea62355e0eca63e198da53cce01fd482
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-06-10 14:25:50 -0500

    Updated File

---
+++ prototype.js
@@ -0,0 +1,8 @@
+let time = new Date();
+let hours = time.getHours();
+console.log(hours)
+let minutes = time.getMinutes();
+console.log(minutes)
+let seconds = time.getSeconds();
+console.log(seconds)
+
