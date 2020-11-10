ed67df3e72ed462a8470e7d1721abae18a6bb6fa
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-11-10 15:58:24 -0600

    Refactored code

--- prototype.js
+++
@@ -1,8 +0,0 @@
-let time = new Date();
-let hours = time.getHours();
-console.log(hours)
-let minutes = time.getMinutes();
-console.log(minutes)
-let seconds = time.getSeconds();
-console.log(seconds)
-
