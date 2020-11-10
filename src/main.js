0cc4f413ad491bdf01841219ca38310ceecbf127
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-11-10 16:07:40 -0600

    Refactored code

--- src/main.js
+++ src/main.js
@@ -1,29 +1,28 @@
 function showTime(){
-    var date = new Date();
-    var h = date.getHours(); 
-    var m = date.getMinutes(); 
-    var s = date.getSeconds(); 
-    var session = "AM";
+    let date = new Date();
+    let hours = date.getHours(); 
+    let minutes = date.getMinutes(); 
+    let seconds = date.getSeconds(); 
+    let session = "AM";
     
-    if(h == 0){
-        h = 12;
+    if (hours == 0) {
+        hours = 12;
     }
     
-    if(h > 12){
-        h = h - 12;
+    if (hours > 12) {
+        hours -=12;
         session = "PM";
     }
     
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
     
-    var time = h + ":" + m + ":" + s + " " + session;
+    let time = hours + ":" + minutes + ":" + seconds + " " + session;
     document.getElementById("Clock").innerText = time;
     document.getElementById("Clock").textContent = time;
     
     setTimeout(showTime, 1000);
-    
 }
  
 showTime();
