47923241e5af7c0d5681531151795119823997e2
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-11-10 15:57:54 -0600

    Refactored code

--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
-// myClockFunction
-
-function displayTime () {
-    let date = new Date();
-    let h = date.getHours();
-    let m = date.getMinutes();
-    let s = date.getSeconds();
-    // main 
-    let time = h + m + s;
-    document.getElementById("theTime").innerText = time;
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); 
+    var m = date.getMinutes(); 
+    var s = date.getSeconds(); 
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("DigitalCLOCK").innerText = time;
+    document.getElementById("DigitalCLOCK").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
 }
-
-setInterval(displayTime,1000);
+ 
+showTime();
